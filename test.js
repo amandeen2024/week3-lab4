ca131763bfa9ab2076ea95d895ee4f0bb6bb6112
Aman Deep Kaur <AmanDeep.Kaur3@metropolia.fi>
2024-09-29 22:13:06 +0530

    Add message here

---
+++ test.js
@@ -0,0 +1,24 @@
+//example1
+
+const array0 = [1, 4, 9, 16];
+
+const cb= function (x) {
+return x * 3;
+}
+
+
+// Pass a function to map
+const map0 = array0.map (cb);
+
+console.log("array0: ", array0);
+console.log("map0: ",map0);
+
+//example2
+
+const words = ["apple", "banana", "cherry", "date"];
+cb2 = function(word){
+    return word.length;
+};
+const lengths = words.map(cb2);
+  
+console.log("lengths:",lengths);
